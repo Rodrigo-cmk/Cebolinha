830d96b1ca00c488274263bca626b557abbc28e2
Rodrigo Alves <rodrigorac.2003@gmail.com>
2021-03-09 15:33:42 -0300

    Atividade Cebolinha

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Cebolinha
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("---Atividade Cebolinha---");
+            Console.WriteLine("Digite algo e será exibido com dislalia, igual o Cebolina da Turma da Mônica.");
+            Console.WriteLine();
+            Console.WriteLine();
+            
+            
+            string troca = Console.ReadLine(); 
+            Console.WriteLine(troca.Replace("r","l").Replace("R","L"));
+            
+
+
+
+
+
+
+        }
+    }
+}
